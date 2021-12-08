d8532fee354bb200917f01743be539849195d5dc
Zymantas Maumevicius <zymantas@billo.app>
2021-12-08 21:36:05 +0200

    Add tls

--- pkg/services/authentication/authorization.go
+++ pkg/services/authentication/authorization.go
@@ -2,11 +2,7 @@ package authentication
 
 import (
 	"context"
-	"crypto/x509"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
 
 	httpclient "github.com/fr0stylo/magistras/common/pkg/internal/httpClient"
 )
@@ -25,14 +21,6 @@ type Client interface {
 }
 
 func init() {
-	caCertPool := x509.NewCertPool()
-	caCert, err := ioutil.ReadFile(os.Getenv("CA_PEM_PATH"))
-	if err != nil {
-		log.Fatalf("Reading server certificate: %s", err)
-	}
-
-	caCertPool.AppendCertsFromPEM(caCert)
-
 	authHttpClient = httpclient.NewClient("http://auth:8000", &http.Transport{})
 }
 
