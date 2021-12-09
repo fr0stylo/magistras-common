e079bca2b2e83e5961f760c6045aeae0d9b37961
Zymantas Maumevicius <zymantas@billo.app>
2021-12-09 15:56:49 +0200

    Update deps

---
+++ pkg/services/courses/courses.go
@@ -0,0 +1,26 @@
+package courses
+
+import (
+	"context"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	httpclient "github.com/fr0stylo/magistras/common/pkg/services/httpClient"
+)
+
+var courseClient Client
+
+type Client interface {
+	Get(ctx context.Context, url string, response interface{}) error
+}
+
+func init() {
+	courseClient = httpclient.NewClient("http://courses:8000", &http.Transport{})
+}
+
+func GerCourse(ctx context.Context, id primitive.ObjectID, course interface{}) error {
+	err := courseClient.Get(ctx, "/"+id.Hex(), course)
+
+	return err
+}
