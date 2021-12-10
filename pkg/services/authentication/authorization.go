3af273f2d2a503a42fef7e8bda1946ff60e97dba
Zymantas Maumevicius <zymantas@billo.app>
2021-12-10 13:31:56 +0200

    Update deps

--- pkg/services/authentication/authorization.go
+++ pkg/services/authentication/authorization.go
@@ -3,6 +3,7 @@ package authentication
 import (
 	"context"
 	"net/http"
+	"strings"
 
 	httpclient "github.com/fr0stylo/magistras/common/pkg/services/httpClient"
 )
@@ -20,13 +21,29 @@ type Client interface {
 	Get(ctx context.Context, url string, response interface{}) error
 }
 
-func init() {
+func NewClient() {
 	authHttpClient = httpclient.NewClient("http://auth:8000", &http.Transport{})
 }
 
 func GetAuthenticatedUser(ctx context.Context) (*User, error) {
+	if authHttpClient != nil {
+		NewClient()
+	}
+
 	var user User
 	err := authHttpClient.Get(ctx, "/authorize", &user)
+
+	return &user, err
+}
+
+func GetUsersByIds(ctx context.Context, ids []string) (*map[string]interface{}, error) {
+	if authHttpClient != nil {
+		NewClient()
+	}
+
+	var user map[string]interface{}
+	queryIds := strings.Join(ids, ",")
+	err := authHttpClient.Get(ctx, "/users?ids="+queryIds, &user)
 
 	return &user, err
 }
